9b2149cd085eddbcc25ce9a79f29a71fcb4a5980
sami-p <sami.peterson14@gmail.com>
2021-05-19 15:49:59 -0500

    Add zip_code instance variable

--- lib/zoo.rb
+++ lib/zoo.rb
@@ -2,12 +2,14 @@ class Zoo
   attr_reader :name,
               :street,
               :city,
-              :state
+              :state,
+              :zip_code
 
   def initialize(name, street, city, state, zip_code)
     @name = name
     @street = street
     @city = city
     @state = state
+    @zip_code = zip_code
   end
 end
