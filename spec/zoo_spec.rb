ee00e9e3e4977ca5776ee69e79af695d5f8f1649
sami-p <sami.peterson14@gmail.com>
2021-05-19 15:35:14 -0500

    Test #initialize of zoo

---
+++ spec/zoo_spec.rb
@@ -0,0 +1,10 @@
+require './lib/animal'
+require './lib/zoo'
+
+RSpec.describe Zoo do
+  it 'exists' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo).to be_an_instance_of(Zoo)
+  end
+end
