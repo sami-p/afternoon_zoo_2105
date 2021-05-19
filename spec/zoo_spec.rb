1e76328cbf425a5e7ed8b4889844ca0a38e84d4c
sami-p <sami.peterson14@gmail.com>
2021-05-19 16:02:17 -0500

    Test add_animals modification to zoo inventory

--- spec/zoo_spec.rb
+++ spec/zoo_spec.rb
@@ -55,4 +55,18 @@ RSpec.describe Zoo do
 
     expect(zoo.animal_count).to eq(0)
   end
+
+  describe 'import animals' do
+    it 'can add animals to inventory' do
+      zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+      animal_1 = Animal.new("Sea Otter", 10, 25)
+      animal_2 = Animal.new("Red Panda", 5, 70)
+
+      zoo.add_animal(animal_1)
+      zoo.add_animal(animal_2)
+
+      expect(zoo.inventory).to eq([animal_1, animal_2])
+    end
+  end
 end
