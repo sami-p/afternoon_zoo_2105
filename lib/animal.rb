37d164f183a88f62ae2e32de80c4966d93650c8c
sami-p <sami.peterson14@gmail.com>
2021-05-19 15:09:27 -0500

    Add #age instance variable

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,9 +1,11 @@
 class Animal
   attr_reader :kind,
-              :weight
+              :weight,
+              :age
 
   def initialize(kind, weight, age)
     @kind = kind
     @weight = weight.to_s + " pounds"
+    @age = age.to_s + " weeks"
   end
 end
