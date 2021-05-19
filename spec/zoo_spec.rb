ef2e81e292183c5d0d65aa71e646fae9e1b5b60f
sami-p <sami.peterson14@gmail.com>
2021-05-19 16:20:02 -0500

    Test #total_weight_of_animals

--- spec/zoo_spec.rb
+++ spec/zoo_spec.rb
@@ -100,5 +100,21 @@ RSpec.describe Zoo do
       expect(zoo.animals_older_than(100)).to eq([animal_3, animal_4])
       expect(zoo.animals_older_than(10)).to eq([animal_1, animal_2, animal_3, animal_4])
     end
+
+    it 'calculates #total_weight_of_animals' do
+      zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+      animal_1 = Animal.new("Sea Otter", 10, 25)
+      animal_2 = Animal.new("Red Panda", 5, 70)
+      animal_3 = Animal.new("Capybara", 100, 150)
+      animal_4 = Animal.new("Dolphin", 150, 200)
+
+      zoo.add_animal(animal_1)
+      zoo.add_animal(animal_2)
+      zoo.add_animal(animal_3)
+      zoo.add_animal(animal_4)
+
+      expect(zoo.total_weight_of_animals).to eq(265)
+    end
   end
 end
