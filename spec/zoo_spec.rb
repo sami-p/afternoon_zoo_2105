816096a25ab55e389ec7b88350ec6faed6f19018
sami-p <sami.peterson14@gmail.com>
2021-05-19 15:41:41 -0500

    Test that zoo has a name

--- spec/zoo_spec.rb
+++ spec/zoo_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Zoo do
 
     expect(zoo).to be_an_instance_of(Zoo)
   end
+
+  it 'has a name' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo.name).to eq("Bronx Zoo")
+  end
 end
