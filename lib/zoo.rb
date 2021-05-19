4d0ad1e732445b73fcb5a6f8d048665f4588d2bd
sami-p <sami.peterson14@gmail.com>
2021-05-19 15:48:32 -0500

    Add state instance variable

--- lib/zoo.rb
+++ lib/zoo.rb
@@ -1,11 +1,13 @@
 class Zoo
   attr_reader :name,
               :street,
-              :city
+              :city,
+              :state
 
   def initialize(name, street, city, state, zip_code)
     @name = name
     @street = street
     @city = city
+    @state = state
   end
 end
